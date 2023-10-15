d93237e5ff7bb37451110adb506c334218d1c3df
AleksHome <aa.polushkin@yandex.ru>
2023-10-15 15:20:29 +0300

    Тестирование работы модели представления

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel :ViewModel
+    {
+        #region Заголовок окна
+
+        private string _title = "Анализ статистики";
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+    }
+}
