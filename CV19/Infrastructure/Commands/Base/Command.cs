d0213afe5f97d4eb630a31c3827d0c14d49a005b
AleksHome <aa.polushkin@yandex.ru>
2023-10-15 18:48:06 +0300

    Базовый класс команды

---
+++ CV19/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CV19.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            // Передаем управление событием классу CommandManager
+            // При этом WPF автоматически генерирует выполнение всех команд, когда возникает событие
+
+            // Добавляем обработчик событий, который пытаются добавить в CanExecuteChanged
+            add => CommandManager.RequerySuggested += value;
+
+            // Удаляем обработчик событий.
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        // Когда CanExecute 
+        public abstract bool CanExecute(object? parameter);
+        
+        public abstract void Execute(object? parameter)
+        
+    }
+}
