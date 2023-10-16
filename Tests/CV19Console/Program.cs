5f26a0ee3712a5911a3d7593c78112095284bbad
AleksHome <aa.polushkin@yandex.ru>
2023-10-16 21:16:55 +0300

    Запрос к удаленному серверу для получения сырых данных

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private const string dataUrl = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    static void Main(string[] args)
+    {
+        var client = new HttpClient();
+
+        var response = client.GetAsync(dataUrl).Result;
+
+        var csvData = response.Content.ReadAsStringAsync().Result;
+
+        Console.WriteLine("Hello, World!");
+    }
+}
