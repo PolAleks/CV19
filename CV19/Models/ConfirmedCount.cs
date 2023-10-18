68bf08e007776935d1a3ce915a012aadd980cfda
AleksHome <aa.polushkin@yandex.ru>
2023-10-18 14:56:24 +0300

    Добавляем модели

---
+++ CV19/Models/ConfirmedCount.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace CV19.Models
+{
+    internal struct ConfirmedCount
+    {
+        // Дата 
+        public DataSetDateTime Date { get; set; }
+
+        // Кол-во заражений на указанную дату
+        public int Count { get; set; }
+    }
+
+}
---
+++ CV19/Models/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.Models
+{
+    internal class PlaceInfo
+    {
+        public string Name { get; set; }
+
+        public Point Location { get; set; }
+
+        // Кол-во подтвержденных случаев
+        public IEnumerable<ConfirmedCount> Counts { get; set; }
+    }
+
+}
